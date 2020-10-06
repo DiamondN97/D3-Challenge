62003a31e3431e3626cc92728240baa92306a3a9
Nate <nathanieldiamond97@gmail.com>
2020-10-06 10:36:05 -0600

    working on margin framework and chart foundation

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,26 @@
 // @TODO: YOUR CODE HERE!
+
+var svgWidth = 960;
+var svgHeight = 660;
+console.log(svgHeight);
+
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 70,
+    left: 70
+};
+
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+var svg = d3
+          .select("#scatter")
+          .append("svg")
+          .attr("height", svgHeight)
+          .attr("width", svgWidth);
+
+
+var chartGroup = svg.append("g")
+                 .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
